de3a10862cdc5fc83f799214e48d795a60c0154a
Medvedev Stanislaw Andreevich <medvedev.sa@edu.spbstu.ru>
2022-11-12 03:55:12 +0300

    recursion

---
+++ Recursion/Recursion.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cctype>
+
+bool isReal(char*& str);
+bool isMantissa(const char*& str);
+bool isExhibitor(const char*& str);
+bool isUnsignedInteger(const char* leftBorder, const char* rightBorder);
+bool isDigit(const char*& str);
+bool isSign(const char*& str);
+char* endOfStr(const char*& str);
+
+const int MAX_INPUT_LENGTH = 256;
+
+int main()
+{
+	std::setlocale(LC_ALL, "Russian");
+	std::cin >> std::setw(MAX_INPUT_LENGTH);
+
+	char* nomber = new char[MAX_INPUT_LENGTH];
+
+	for (;;)
+	{
+		std::cout << "Введите вещественное число в экспоненциальной форме записи : ";
+		
+		std::cin >> nomber;
+
+		std::cout << isReal(nomber) ? "Правда\n\n" : "Ложь\n\n";
+	}
+
+	delete[] nomber;
+}
+
+bool isReal(const char *& str)
+{
+	isSign(str);
+
+	return isMantissa(str) && isExhibitor(str) && *str == '\0';
+}
+
+bool isMantissa(const char*& str)
+{
+	const char* rightBorder = endOfStr(str);
+	const char* EPosition = str - 1;
+	if (*EPosition == 'E')
+	{
+		return (isSign(str) && (str + 1 != rightBorder)) ? (isUnsignedInteger(str + 1, rightBorder)) : false;
+	}
+}
+
+bool isExhibitor(const char*& str)
+{
+
+}
+
+bool isUnsignedInteger(const char* leftBorder, const char* rightBorder)
+{
+	if (leftBorder == rightBorder)
+	{
+		return true;
+	}
+	if (isdigit(*leftBorder))
+	{
+		return isUnsignedInteger(leftBorder + 1, rightBorder);
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool isDigit(const char*& str)
+{
+	return std::isdigit(*str);
+}
+
+bool isSign(const char*& str)
+{
+	return (*str == '-' || *str == '+');
+}
+
+char* endOfStr(char* num)
+{
+	return (*num == '\0') ? num : endOfStr(num + 1);
+}
--- arrows/arrows.cpp
+++ arrows/arrows.cpp
@@ -1,40 +1,94 @@
 ﻿#include <iostream>
-#include <chrono>
-#include <thread>
+#include <iomanip>
 
-class MemoryLeaker
+
+bool isReal(char* num);
+bool isMantissa(char* num, char* EPosition);
+bool isExponent(char* num);
+bool isUnsignedInt(char* leftBorder, char* rightBorder);
+bool isSign(char* num);
+char* endOfStr(char* num);
+
+
+bool isReal(char* num)
 {
-private:
-	int* _leakerArray = nullptr;
-	int _length = 0;
-
-public:
-	MemoryLeaker()
-	{
-		srand((unsigned)time(NULL) / 2);
-		_length = rand() % 10001;
-		_leakerArray = new int[_length];
-
-		for (size_t i = 0; i < _length; i++)
-		{
-			_leakerArray[i] = (rand() % 10001);
-		}
-	}
-};
-
-
-int main()
-{
-	using namespace std::this_thread;
-	using namespace std::chrono_literals;
-
-	int i = 0;
-	while (i++ < 10);
-	{
-		new MemoryLeaker();
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	}
-
-	return 0;
+    char* EPosition = strstr(num, "E");
+    if (!EPosition)
+        EPosition = strstr(num, "e");
+    if (isdigit(*num) or *num == '.')
+        return (isMantissa(num, EPosition) and isExponent(EPosition + 1));
+    else
+        return false;
 }
 
+bool isMantissa(char* num, char* EPosition)
+{
+    char* dotPosition = strstr(num, ".");
+    if (dotPosition + 1 == EPosition)
+        return false;
+    if (isUnsignedInt(num, dotPosition) and isUnsignedInt(dotPosition + 1, EPosition))
+        return true;
+    else
+        return false;
+}
+
+bool isUnsignedInt(char* leftBorder, char* rightBorder)
+{
+    if (leftBorder == rightBorder)
+        return true;
+    if (isdigit(*leftBorder))
+        return isUnsignedInt(leftBorder + 1, rightBorder);
+    else
+        return false;
+}
+
+bool isExponent(char* num)
+{
+    char* rightBorder = endOfStr(num);
+    char* EPosition = num - 1;
+    if (*EPosition == 'E')
+    {
+        if (isSign(num) and (num + 1 != rightBorder))
+            return (isUnsignedInt(num + 1, rightBorder));
+        else
+            return false;
+    }
+    else if (*EPosition == 'e')
+    {
+        if (isdigit(*num))
+            return (isUnsignedInt(num, rightBorder));
+        else
+            return false;
+    }
+    else
+        return false;
+}
+
+char* endOfStr(char* num)
+{
+    if (*num == '\0')
+        return (num);
+    return endOfStr(num + 1);
+}
+
+bool isSign(char* num)
+{
+    if (*num == '-' or *num == '+')
+        return true;
+    else
+        return false;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    while (true)
+    {
+        char* num = new char[100]();
+        std::cout << "Введите вещественное число: ";
+        std::cin >> std::setw(100) >> num;
+        if (isReal(num))
+            std::cout << "Это вещественное число\n";
+        else
+            std::cout << "Это не вещественное число\n";
+    }
+}
